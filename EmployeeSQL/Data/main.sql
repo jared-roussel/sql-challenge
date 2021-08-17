386e523ed8d6495afa0af085ebe1476c19189658
jared.roussel@outlook.com <jared.roussel@outlook.com>
2021-08-17 00:08:13 -0400

    Completed analysis

--- EmployeeSQL/Data/main.sql
+++ EmployeeSQL/Data/main.sql
@@ -99,14 +99,41 @@ JOIN departments as d ON (d.dept_no = dm.dept_no)
 JOIN employees as e ON (e.emp_no = dm.emp_no)
 ;
 
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name--, d.dept_name
+FROM employees as e
+JOIN dept_emp as de
+ON CAST (e.emp_no AS VARCHAR) = de.emp_no;
+--JOIN employees as e ON (e.emp_no = dm.emp_no)
+;
 
 
+--List first name, last name, and sex for employees whose first name 
+--is "Hercules" and last names begin with "B."
 
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
+--List all employees in the Sales department, including their 
+--employee number, last name, first name, and department name.
 
+SELECT emp_no, last_name, first_name, dept_name
+FROM employees
+JOIN departments ON 
 
+--List all employees in the Sales and Development departments, including their 
+--employee number, last name, first name, and department name.
 
+--In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
 
+SELECT last_name, COUNT(last_name) as "Frequency Count of Employee Last Name" 
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency Count of Employee Last Name" DESC;
 
 
 
