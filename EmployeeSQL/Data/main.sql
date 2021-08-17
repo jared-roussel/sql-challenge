b37ed534bd3f3a81d75412744b7148b480734b32
jared.roussel@outlook.com <jared.roussel@outlook.com>
2021-08-16 23:41:55 -0400

    Updated script

--- EmployeeSQL/Data/main.sql
+++ EmployeeSQL/Data/main.sql
@@ -77,16 +77,27 @@ ON (e.emp_no = s.emp_no)
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
 
+SELECT first_name, last_name, hire_date
+FROM employees 
+--WHERE hire_date LIKE '1986%';
+WHERE CAST(hire_date AS VARCHAR) LIKE '1986%';
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
 
 
 
+SELECT d.dept_no, d.dept_name, dm.emp_no --, e.last_name, e.first_name
+FROM dept_manager as dm
+JOIN departments as d
+ON (d.dept_no = dm.dept_no)
+;
 
-
-
-
-
-
-
+SELECT d.dept_no, d.dept_name, dm.emp_no , e.last_name, e.first_name
+FROM dept_manager as dm
+JOIN departments as d ON (d.dept_no = dm.dept_no)
+JOIN employees as e ON (e.emp_no = dm.emp_no)
+;
 
 
 
